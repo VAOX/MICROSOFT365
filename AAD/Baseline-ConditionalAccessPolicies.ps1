5c5f53d6d9ca5f35fb8e75f65c5a9e46d3081b66
VAOX <tony@pemberton.me>
2021-05-06 16:49:07 +0100

    Update Baseline-ConditionalAccessPolicies.ps1

--- AAD/Baseline-ConditionalAccessPolicies.ps1
+++ AAD/Baseline-ConditionalAccessPolicies.ps1
@@ -21,9 +21,9 @@
     4. Reference: https://docs.microsoft.com/en-us/powershell/azure/active-directory/install-adv2?view=azureadps-2.0#installing-the-azure-ad-module
 .DETAILS
     FileName:    Baseline-ConditionalAccessPolicies.ps1
-    Author:      Alex Fields, ITProMentor.com
-    Created:     September 2020
-    Updated:     February 2021
+    Author:      Tony Pemberton
+    Created:     May 2021
+    Updated:     
 #>
 ###################################################################################################
 
